d32e36d5b35a1da1cc8f0f037588f1d484278b52
Masooka <masooka@live.com>
2022-12-27 20:43:10 +0000

    Day 4: Use nom for parsing

--- aoc04/src/main.rs
+++ aoc04/src/main.rs
@@ -1,69 +1,87 @@
-use std::io::{self, BufRead};
-use std::str::FromStr;
+use std::{
+    io::{self, Read},
+    ops::RangeInclusive,
+    str::FromStr,
+};
 
-use anyhow::Result;
-
-/// An inclusive interval.
-struct Interval {
-    start: u32,
-    end: u32,
-}
-
-impl Interval {
-    /// Returns `true` if this interval contains the given interval.
-    fn includes(&self, other: &Interval) -> bool {
-        self.start <= other.start && other.end <= self.end
-    }
-
-    /// Returns `true` if this interval overlaps the given interval.
-    fn overlaps(&self, other: &Interval) -> bool {
-        self.start <= other.end && other.start <= self.end
-    }
-}
-
-impl FromStr for Interval {
-    type Err = anyhow::Error;
-
-    fn from_str(s: &str) -> Result<Self> {
-        let mut parts = s.splitn(2, '-');
-        let start = parts
-            .next()
-            .ok_or_else(|| anyhow::anyhow!("missing start"))?
-            .parse()?;
-        let end = parts
-            .next()
-            .ok_or_else(|| anyhow::anyhow!("missing end"))?
-            .parse()?;
-        Ok(Interval { start, end })
-    }
-}
+use anyhow::{anyhow, Result};
+use itertools::process_results;
+use nom::{character::complete, combinator::map_res, sequence::separated_pair, IResult};
 
 fn main() -> Result<()> {
-    let stdin = io::stdin();
-    let lines = io::BufReader::new(stdin).lines();
-
-    let mut include_count = 0;
-    let mut overlap_count = 0;
-    for line in lines {
-        let line = line?;
-        let mut parts = line.splitn(2, ',');
-        let first = parts
-            .next()
-            .ok_or_else(|| anyhow::anyhow!("missing first"))?
-            .parse::<Interval>()?;
-        let second = parts
-            .next()
-            .ok_or_else(|| anyhow::anyhow!("missing second"))?
-            .parse::<Interval>()?;
-        if first.includes(&second) || second.includes(&first) {
-            include_count += 1;
-        }
-        if first.overlaps(&second) {
-            overlap_count += 1;
-        }
-    }
-
-    println!("Part 1: {}", include_count);
-    println!("Part 2: {}", overlap_count);
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+    println!("Part 1: {}", part1(&input)?);
+    println!("Part 2: {}", part2(&input)?);
     Ok(())
+}
+
+fn parse<Idx: FromStr>(
+    input: &str,
+) -> impl Iterator<Item = Result<(RangeInclusive<Idx>, RangeInclusive<Idx>)>> + '_ {
+    input.lines().map(|line| {
+        let (input, (start, end)) = assignment_pair(line)
+            .map_err(|e| anyhow!("failed to parse line \"{}\": {}", line, e))?;
+        if !input.is_empty() {
+            return Err(anyhow!("unexpected input \"{}\"", input));
+        }
+        Ok((start, end))
+    })
+}
+
+fn assignment_pair<Idx: FromStr>(
+    input: &str,
+) -> IResult<&str, (RangeInclusive<Idx>, RangeInclusive<Idx>)> {
+    separated_pair(interval, complete::char(','), interval)(input)
+}
+
+fn interval<Idx: FromStr>(input: &str) -> IResult<&str, RangeInclusive<Idx>> {
+    let (input, (start, end)) = separated_pair(
+        map_res(complete::digit1, str::parse),
+        complete::char('-'),
+        map_res(complete::digit1, str::parse),
+    )(input)?;
+    Ok((input, RangeInclusive::new(start, end)))
+}
+
+fn part1(input: &str) -> Result<usize> {
+    process_results(parse::<u64>(input), |iter| {
+        iter.filter(|(a, b)| includes(a, b) || includes(b, a))
+            .count()
+    })
+}
+
+fn part2(input: &str) -> Result<usize> {
+    process_results(parse::<u64>(input), |iter| {
+        iter.filter(|(a, b)| overlaps(a, b)).count()
+    })
+}
+
+/// Returns `true` if range `a` includes range `b`.
+fn includes<Idx: PartialOrd>(a: &RangeInclusive<Idx>, b: &RangeInclusive<Idx>) -> bool {
+    a.start() <= b.start() && b.end() <= a.end()
+}
+
+/// Returns `true` if range `a` overlaps range `b`.
+fn overlaps<Idx: PartialOrd>(a: &RangeInclusive<Idx>, b: &RangeInclusive<Idx>) -> bool {
+    a.start() <= b.end() && b.start() <= a.end()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        const INPUT: &str = "\
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+";
+        assert_eq!(part1(INPUT).unwrap(), 2);
+        assert_eq!(part2(INPUT).unwrap(), 4);
+    }
 }
