2e311f5bb99e9ba6c0536fc10217461756fd6200
Masooka <masooka@live.com>
2022-12-28 18:05:55 +0000

    Day 2: Use num_enum

--- aoc02/src/main.rs
+++ aoc02/src/main.rs
@@ -1,51 +1,62 @@
-use std::io::{self, BufRead};
+use std::io::{self, Read};
 
-use anyhow::{bail, Result};
+use anyhow::{anyhow, bail, Error, Result};
+use itertools::process_results;
+use nom::{
+    character::complete::{self, one_of},
+    sequence::separated_pair,
+    IResult,
+};
+use num_enum::{IntoPrimitive, TryFromPrimitive};
 
 /// An outcome of a single round of the game.
-///
-/// * 0: draw
-/// * 1: win
-/// * 2: loss
-#[derive(Clone, Copy)]
-struct Outcome(u8);
+#[derive(Clone, Copy, IntoPrimitive)]
+#[repr(u8)]
+enum Outcome {
+    Draw = 0,
+    Win = 1,
+    Loss = 2,
+}
 
 impl TryFrom<char> for Outcome {
-    type Error = anyhow::Error;
+    type Error = Error;
 
     fn try_from(value: char) -> Result<Self> {
-        let outcome = match value {
-            'X' => 2,
-            'Y' => 0,
-            'Z' => 1,
+        Ok(match value {
+            'X' => Outcome::Loss,
+            'Y' => Outcome::Draw,
+            'Z' => Outcome::Win,
             _ => bail!("invalid outcome: {}", value),
-        };
-
-        Ok(Outcome(outcome))
+        })
     }
 }
 
-/// A choice from rock (0), paper (1), scissors (2).
-#[derive(Clone, Copy)]
-struct Choice(u8);
+/// A choice from rock, paper, scissors.
+#[derive(Clone, Copy, IntoPrimitive, TryFromPrimitive)]
+#[repr(u8)]
+enum Choice {
+    Rock = 0,
+    Paper = 1,
+    Scissors = 2,
+}
 
 impl Choice {
     /// Returns the choice to make to achieve the goal.
     fn plan(self, goal: Outcome) -> Choice {
-        Choice((self.0 + goal.0) % 3)
+        ((u8::from(self) + u8::from(goal)) % 3).try_into().unwrap()
     }
 }
 
 impl TryFrom<char> for Choice {
-    type Error = anyhow::Error;
+    type Error = Error;
 
     fn try_from(value: char) -> Result<Self> {
-        match value {
-            'A' | 'X' => Ok(Choice(0)),
-            'B' | 'Y' => Ok(Choice(1)),
-            'C' | 'Z' => Ok(Choice(2)),
+        Ok(match value {
+            'A' => Choice::Rock,
+            'B' => Choice::Paper,
+            'C' => Choice::Scissors,
             _ => bail!("invalid choice: {}", value),
-        }
+        })
     }
 }
 
@@ -60,44 +71,72 @@ struct Round {
 impl Round {
     /// Returns the score for the player.
     fn score(&self) -> usize {
-        let outcome_score = match (self.player.0 + 3 - self.opponent.0) % 3 {
+        let outcome_score = match (u8::from(self.player) + 3 - u8::from(self.opponent)) % 3 {
             0 => 3,
             1 => 6,
             2 => 0,
             _ => unreachable!(),
         };
-        let selection_score = self.player.0 as usize + 1;
+        let selection_score = u8::from(self.player) as usize + 1;
         outcome_score + selection_score
     }
 }
 
 fn main() -> Result<()> {
-    let stdin = io::stdin();
-    let lines = io::BufReader::new(stdin).lines();
-
-    let mut score1 = 0;
-    let mut score2 = 0;
-    for line in lines {
-        let line = line?;
-        let mut chars = line.chars();
-        let opponent = chars.next().unwrap().try_into()?;
-        let second_col = chars.next_back().unwrap();
-        let round = Round {
-            opponent,
-            player: second_col.try_into()?,
-        };
-        score1 += round.score();
-
-        let round = Round {
-            opponent,
-            player: opponent.plan(second_col.try_into()?),
-        };
-        score2 += round.score();
-    }
-
-    println!("Part 1: {}", score1);
-    println!("Part 2: {}", score2);
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+    println!("Part 1: {}", part1(&input)?);
+    println!("Part 2: {}", part2(&input)?);
     Ok(())
+}
+
+fn parse(input: &str) -> impl Iterator<Item = Result<(char, char)>> + '_ {
+    input.lines().map(|line| {
+        let (input, (opponent, player)) = strategy_guide(line)
+            .map_err(|e| anyhow!("failed to parse line: \"{}\": {}", line, e))?;
+        if !input.is_empty() {
+            bail!(
+                "failed to parse line: \"{}\": leftover input: {}",
+                line,
+                input
+            );
+        }
+        Ok((opponent, player))
+    })
+}
+
+fn strategy_guide(input: &str) -> IResult<&str, (char, char)> {
+    separated_pair(one_of("ABC"), complete::char(' '), one_of("XYZ"))(input)
+}
+
+fn part1(input: &str) -> Result<usize> {
+    process_results(parse(input), |iter| {
+        iter.map(|(opponent, player)| {
+            let player =
+                char::from_u32(u32::from(player) + u32::from('A') - u32::from('X')).unwrap();
+            Round {
+                opponent: opponent.try_into().unwrap(),
+                player: player.try_into().unwrap(),
+            }
+            .score()
+        })
+        .sum()
+    })
+}
+
+fn part2(input: &str) -> Result<usize> {
+    process_results(parse(input), |iter| {
+        iter.map(|(opponent, goal)| {
+            let opponent = opponent.try_into().unwrap();
+            let goal = goal.try_into().unwrap();
+            Round {
+                opponent,
+                player: opponent.plan(goal),
+            }
+            .score()
+        })
+        .sum()
+    })
 }
 
 #[cfg(test)]
@@ -105,23 +144,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_score() {
-        let round = Round {
-            player: 'Y'.try_into().unwrap(),
-            opponent: 'A'.try_into().unwrap(),
-        };
-        assert_eq!(round.score(), 8);
-
-        let round = Round {
-            player: 'X'.try_into().unwrap(),
-            opponent: 'B'.try_into().unwrap(),
-        };
-        assert_eq!(round.score(), 1);
-
-        let round = Round {
-            player: 'Z'.try_into().unwrap(),
-            opponent: 'C'.try_into().unwrap(),
-        };
-        assert_eq!(round.score(), 6);
+    fn example() {
+        const INPUT: &str = "\
+A Y
+B X
+C Z
+";
+        assert_eq!(part1(INPUT).unwrap(), 15);
+        assert_eq!(part2(INPUT).unwrap(), 12);
     }
 }
