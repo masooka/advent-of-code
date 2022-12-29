b27cea6471d9602aaa65c2ca4bcaa3514984d06c
Masooka <masooka@live.com>
2022-12-29 17:48:59 +0000

    Day 6: Add tests

--- 2022/day06/src/main.rs
+++ 2022/day06/src/main.rs
@@ -3,19 +3,15 @@ use std::io::{self, Read};
 use anyhow::Result;
 
 fn main() -> Result<()> {
-    let stdin = io::stdin();
-    let mut reader = io::BufReader::new(stdin);
-    let mut buf = Vec::new();
-    reader.read_to_end(&mut buf)?;
-
-    println!("Part 1: {}", start_of_packet(&buf, 4));
-    println!("Part 2: {}", start_of_packet(&buf, 14));
-
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+    println!("Part 1: {}", start_of_packet(input.as_bytes(), 4));
+    println!("Part 2: {}", start_of_packet(input.as_bytes(), 14));
     Ok(())
 }
 
-fn start_of_packet(data: &[u8], marker_len: usize) -> usize {
-    for (i, seq) in data.windows(marker_len).enumerate() {
+fn start_of_packet(input: &[u8], marker_len: usize) -> usize {
+    for (i, seq) in input.windows(marker_len).enumerate() {
         let set = seq
             .iter()
             .copied()
@@ -25,4 +21,32 @@ fn start_of_packet(data: &[u8], marker_len: usize) -> usize {
         }
     }
     0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        const INPUT1: &str = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+        const INPUT2: &str = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+        const INPUT3: &str = "nppdvjthqldpwncqszvftbrmjlhg";
+        const INPUT4: &str = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+        const INPUT5: &str = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+
+        assert_eq!(start_of_packet(INPUT1.as_bytes(), 14), 19);
+
+        assert_eq!(start_of_packet(INPUT2.as_bytes(), 4), 5);
+        assert_eq!(start_of_packet(INPUT2.as_bytes(), 14), 23);
+
+        assert_eq!(start_of_packet(INPUT3.as_bytes(), 4), 6);
+        assert_eq!(start_of_packet(INPUT3.as_bytes(), 14), 23);
+
+        assert_eq!(start_of_packet(INPUT4.as_bytes(), 4), 10);
+        assert_eq!(start_of_packet(INPUT4.as_bytes(), 14), 29);
+
+        assert_eq!(start_of_packet(INPUT5.as_bytes(), 4), 11);
+        assert_eq!(start_of_packet(INPUT5.as_bytes(), 14), 26);
+    }
 }
