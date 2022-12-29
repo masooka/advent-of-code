13431c608c88ae3853825e21bc40159e475e04a5
Masooka <masooka@live.com>
2022-12-29 17:40:19 +0000

    Day 3: Add a unit test

--- 2022/day03/src/main.rs
+++ 2022/day03/src/main.rs
@@ -1,7 +1,8 @@
-use std::io::{self, BufRead};
+use std::io::{self, Read};
 
 use anyhow::{bail, Result};
 use bit_set::BitSet;
+use itertools::process_results;
 
 struct Compartment {
     items: BitSet,
@@ -54,39 +55,69 @@ impl Rucksack {
     fn misplaced(&self) -> Option<usize> {
         self.left.items.intersection(&self.right.items).next()
     }
+
+    fn union(&self) -> BitSet {
+        let mut union = self.left.items.clone();
+        union.union_with(&self.right.items);
+        union
+    }
 }
 
 fn main() -> Result<()> {
-    let stdin = io::stdin();
-    let lines = io::BufReader::new(stdin).lines();
-
-    let mut sum = 0;
-    let mut badge_sum = 0;
-    let mut common_items = BitSet::new();
-    for (group, line) in lines.enumerate() {
-        let line = line?;
-        let rucksack = Rucksack::pack(&line)?;
-        let misplaced = rucksack.misplaced();
-        sum += misplaced.unwrap_or(0);
-
-        let mut combined = rucksack.left.items.clone();
-        combined.union_with(&rucksack.right.items);
-        if group % 3 == 0 {
-            common_items = combined;
-        } else {
-            common_items.intersect_with(&combined);
-            if group % 3 == 2 {
-                let badge = common_items
-                    .iter()
-                    .next()
-                    .ok_or_else(|| anyhow::anyhow!("no badge found"))?;
-                println!("Group {}: {}", group, badge);
-                badge_sum += badge;
-            }
-        }
-    }
-
-    println!("Part 1: {}", sum);
-    println!("Part 2: {}", badge_sum);
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+    println!("Part 1: {}", part1(&input)?);
+    println!("Part 2: {}", part2(&input)?);
     Ok(())
+}
+
+fn parse(input: &str) -> impl Iterator<Item = Result<Rucksack>> + '_ {
+    input.lines().map(Rucksack::pack)
+}
+
+fn part1(input: &str) -> Result<usize> {
+    parse(input)
+        .map(|r| r.map(|r| r.misplaced().unwrap_or(0)))
+        .sum()
+}
+
+fn part2(input: &str) -> Result<usize> {
+    process_results(parse(input), |iter| {
+        iter.enumerate()
+            .fold(
+                (0, BitSet::new()),
+                |(mut badge_sum, mut common_items), (group, r)| {
+                    let combined = r.union();
+                    if group % 3 == 0 {
+                        common_items = combined;
+                    } else {
+                        common_items.intersect_with(&combined);
+                        if group % 3 == 2 {
+                            let badge = common_items.iter().next().expect("input should be valid");
+                            badge_sum += badge;
+                        }
+                    }
+                    (badge_sum, common_items)
+                },
+            )
+            .0
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        const INPUT: &str = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+";
+        assert_eq!(part1(INPUT).unwrap(), 157);
+        assert_eq!(part2(INPUT).unwrap(), 70);
+    }
 }
