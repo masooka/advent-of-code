9a76d2221a34afcf442ebd5632607a4bb845e6c1
Masooka <masooka@live.com>
2022-12-31 03:10:08 +0000

    Day 7: Add tests

--- 2022/day07/src/main.rs
+++ 2022/day07/src/main.rs
@@ -1,20 +1,22 @@
 use std::collections::HashMap;
-use std::io;
+use std::io::{self, Read};
 use std::path::PathBuf;
 
 use anyhow::Result;
 
 fn main() -> Result<()> {
-    let lines = io::read_to_string(io::stdin())?
-        .split('\n')
-        .map(ToString::to_string)
-        .collect::<Vec<_>>();
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+    Ok(())
+}
 
+fn parse(input: &str) -> HashMap<PathBuf, usize> {
     let mut cur = PathBuf::new();
     let mut size = 0;
-    let iter = lines.iter();
     let mut sizes = HashMap::<PathBuf, usize>::new();
-    for line in iter {
+    for line in input.lines() {
         let tokens = line.split(' ').collect::<Vec<_>>();
         if tokens[0] == "$" {
             if tokens[1] == "cd" {
@@ -33,7 +35,6 @@ fn main() -> Result<()> {
         }
     }
     *sizes.entry(cur.clone()).or_default() += size;
-
     let mut tree_sizes = sizes.clone();
     for (path, size) in sizes {
         let mut cur = path;
@@ -43,20 +44,56 @@ fn main() -> Result<()> {
         }
     }
 
-    let mut sum = 0;
-    for &size in tree_sizes.values() {
-        if size <= 100_000 {
-            sum += size;
-        }
-    }
-    println!("Part 1: {}", sum);
+    tree_sizes
+}
 
-    let target = 30_000_000 - (70_000_000 - tree_sizes[&PathBuf::from("/")]);
-    let min = tree_sizes
+fn part1(input: &str) -> usize {
+    let sizes = parse(input);
+    sizes.values().filter(|&s| *s <= 100_000).sum()
+}
+
+fn part2(input: &str) -> usize {
+    let sizes = parse(input);
+    let target = 30_000_000 - (70_000_000 - sizes[&PathBuf::from("/")]);
+    let min = sizes
         .values()
         .filter(|&size| size >= &target)
         .min()
         .unwrap();
-    println!("Part 2: {}", min);
-    Ok(())
+    *min
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        const INPUT: &str = r"$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+";
+        assert_eq!(part1(INPUT), 95_437);
+        assert_eq!(part2(INPUT), 24_933_642);
+    }
 }
